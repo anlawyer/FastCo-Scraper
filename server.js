8efb021859b606447239b4a6da16729aa52a680a
Alison Lawyer <alison@Alisons-MacBook-Air.local>
2017-11-21 21:38:21 -0800

    update, still buggy

--- server.js
+++ server.js
@@ -2,15 +2,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var exphbs = require('express-handlebars');
 
-var db = require('./models');
+// var db = require('./models');
 
 var app = express();
 
 var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
@@ -20,6 +21,9 @@ mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/WebScraper', {
   useMongoClient: true
 });
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 require('./routes/api-routes.js')(app);
 
